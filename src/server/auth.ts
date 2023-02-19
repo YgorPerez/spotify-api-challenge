ca57aa51c1840ed4d032b6b06cb394a8c803c0db
ygorperez <perezygor@gmail.com>
2023-02-19 18:05:59 -0300

    added refresh token function and removed JWT callback

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return */
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { GetServerSidePropsContext } from "next";
 import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
+	getServerSession,
+	TokenSet,
+	type DefaultSession,
+	type NextAuthOptions,
 } from "next-auth";
-import { type JWT } from "next-auth/jwt";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { env } from "../env.mjs";
 import { prisma } from "./db";
@@ -21,18 +18,18 @@ import { prisma } from "./db";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  **/
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+	interface Session extends DefaultSession {
+		user: {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession["user"];
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -42,104 +39,91 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  **/
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user, token }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      } 
-      if (token) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        session.accessToken = token.accessToken;
-      }
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      session.error = token?.error;
+	callbacks: {
+		async session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			const [spotify] = await prisma.account.findMany({
+				where: { userId: user.id },
+			});
+			if (!spotify?.expires_at) return session;
 
-      return session;
-    },
-    jwt({ token, user, account }) {
-      console.log({ token });
-      console.log({ user });
-      console.log({ account });
-      if (account && user) {
-        return {
-          accessToken: account.access_token,
-          accessTokenExpires: Date.now() + (account.expires_at ?? 0) * 1000,
-          refreshToken: account.refresh_token,
-          user,
-        };
-      }
+			if (spotify.expires_at < Math.floor(Date.now() / 1000)) {
+				// the token has expired
+				try {
+					const spotifyTokenResponse = await fetch(
+						"https://accounts.spotify.com/api/token",
+						{
+							headers: {
+								"Content-Type":
+									"application/x-www-form-urlencoded",
+							},
+							body: new URLSearchParams({
+								client_id: process.env
+									.SPOTIFY_CLIENT_ID as string,
+								client_secret: process.env
+									.SPOTIFY_CLIENT_SECRET as string,
+								grant_type: "refresh_token",
+								refresh_token: spotify.refresh_token ?? "",
+							}),
+							method: "POST",
+						}
+					);
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      if (Date.now() < token.accessTokenExpires) {
-        return token;
-      }
+					const tokens: TokenSet = await spotifyTokenResponse.json();
 
-      return refreshAccessToken(token);
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    SpotifyProvider({
-      clientId: env.SPOTIFY_CLIENT_ID,
-      clientSecret: env.SPOTIFY_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: `user-read-currently-playing playlist-read-collaborative
+					if (!spotifyTokenResponse.ok) throw tokens;
+
+					console.log("Refreshed access token", tokens);
+
+					await prisma.account.update({
+						data: {
+							access_token: tokens.access_token,
+							expires_at: Date.now() + 3600000,
+							refresh_token:
+								tokens.refresh_token ?? spotify.refresh_token,
+						},
+						where: {
+							provider_providerAccountId: {
+								provider: "spotify",
+								providerAccountId: spotify.providerAccountId,
+							},
+						},
+					});
+				} catch (error) {
+					console.error("Error refreshing access token", error);
+				}
+			}
+			return session;
+		},
+	},
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		SpotifyProvider({
+			clientId: env.SPOTIFY_CLIENT_ID,
+			clientSecret: env.SPOTIFY_CLIENT_SECRET,
+			authorization: {
+				params: {
+					scope: `user-read-currently-playing playlist-read-collaborative
  user-read-recently-played user-top-read user-read-email playlist-read-private
 `,
-        },
-      },
-    }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Spotify provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
-  ],
-  //   pages: {
-  //     signIn: "/auth/signin",
-  //   },
-};
-
-const refreshAccessToken = async (token: JWT) => {
-  console.log({ token });
-  try {
-    const url = "https://accounts.spotify.com/v1/refresh";
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const query = new URLSearchParams({
-      refresh_token: token.refreshToken ?? "",
-    });
-
-    const response = await fetch(url + query, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      method: "POST",
-    });
-
-    const tokens = await response.json();
-    return {
-      ...token,
-      accessToken: tokens.access_token,
-      refreshToken: tokens.refresh_token ?? token.refreshToken,
-    };
-  } catch (error) {
-    console.log({ error });
-
-    return {
-      ...token,
-      error: "refreshAccessTokenError",
-    };
-  }
+				},
+			},
+		}),
+		/**
+		 * ...add more providers here
+		 *
+		 * Most other providers require a bit more work than the Spotify provider.
+		 * For example, the GitHub provider requires you to add the
+		 * `refresh_token_expires_in` field to the Account model. Refer to the
+		 * NextAuth.js docs for the provider you want to use. Example:
+		 * @see https://next-auth.js.org/providers/github
+		 **/
+	],
+	//   pages: {
+	//     signIn: "/auth/signin",
+	//   },
 };
 
 /**
@@ -149,8 +133,8 @@ const refreshAccessToken = async (token: JWT) => {
  * @see https://next-auth.js.org/configuration/nextjs
  **/
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+	req: GetServerSidePropsContext["req"];
+	res: GetServerSidePropsContext["res"];
 }) => {
-  return getServerSession(ctx.req, ctx.res, authOptions);
+	return getServerSession(ctx.req, ctx.res, authOptions);
 };
