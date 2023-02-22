69ce7a5c9c3385577a8f99161dc81b123ebf7feb
ygorperez <perezygor@gmail.com>
2023-02-21 21:03:07 -0300

    moved accessToken to session and changed formating

--- src/server/api/routers/spotify.ts
+++ src/server/api/routers/spotify.ts
@@ -2,65 +2,65 @@ import { Cache, Client } from "spotify-api.js";
 import { z } from "zod";
 
 import {
-  createTRPCRouter,
-  protectedProcedure,
-  protectedTokenProcedure,
-  publicProcedure,
+	createTRPCRouter,
+	protectedProcedure,
+	protectedTokenProcedure,
+	publicProcedure,
 } from "../trpc";
 
 export const spotifyRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
-  getSpotifyPlaylist: protectedTokenProcedure
-    .input(z.object({ playlistId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const client = new Client({
-        token: ctx.session.user.accessToken,
-        cacheSettings: {
-          playlists: true,
-        },
-        retryOnRateLimit: true,
-      });
-      const playlist = await client.playlists.get(input.playlistId);
-      Cache.playlists.get(input.playlistId)
-      return playlist ;
-    }),
-  getSpotifyAlbum: protectedTokenProcedure
-    .input(z.object({ albumId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const client = new Client({
-        token: ctx.session.user.accessToken,
-        cacheSettings: {
-          albums: true,
-        },
-        retryOnRateLimit: true,
-      });
-      const album = await client.albums.get(input.albumId);
-      Cache.albums.get(input.albumId);
-      // cache the album
-      return album;
-    }),
-  getSpotifySearchAlbum: protectedTokenProcedure
-    .input(z.object({ searchQuery: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const client = new Client({
-        token: ctx.session.user.accessToken,
-        retryOnRateLimit: true,
-      });
-      const { tracks, albums, artists } = await client.search(
-        input.searchQuery,
-        {
-          types: ["track", "album", "artist"],
-        }
-      );
-      return { tracks, albums, artists };
-    }),
+	hello: publicProcedure
+		.input(z.object({ text: z.string() }))
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input.text}`,
+			};
+		}),
+	getSecretMessage: protectedProcedure.query(() => {
+		return "you can now see this secret message!";
+	}),
+	getSpotifyPlaylist: protectedTokenProcedure
+		.input(z.object({ playlistId: z.string() }))
+		.query(async ({ ctx, input }) => {
+			const client = new Client({
+				token: ctx.session.user.accessToken,
+				cacheSettings: {
+					playlists: true,
+				},
+				retryOnRateLimit: true,
+			});
+			const playlist = await client.playlists.get(input.playlistId);
+			Cache.playlists.get(input.playlistId);
+			return playlist;
+		}),
+	getSpotifyAlbum: protectedTokenProcedure
+		.input(z.object({ albumId: z.string() }))
+		.query(async ({ ctx, input }) => {
+			const client = new Client({
+				token: ctx.session.user.accessToken,
+				cacheSettings: {
+					albums: true,
+				},
+				retryOnRateLimit: true,
+			});
+			const album = await client.albums.get(input.albumId);
+			Cache.albums.get(input.albumId);
+			// cache the album
+			return album;
+		}),
+	getSpotifySearchAlbum: protectedTokenProcedure
+		.input(z.object({ searchQuery: z.string() }))
+		.query(async ({ ctx, input }) => {
+			const client = new Client({
+				token: ctx.session.user.accessToken,
+				retryOnRateLimit: true,
+			});
+			const { tracks, albums, artists } = await client.search(
+				input.searchQuery,
+				{
+					types: ["track", "album", "artist"],
+				}
+			);
+			return { tracks, albums, artists };
+		}),
 });
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,9 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { GetServerSidePropsContext } from "next";
 import {
-  getServerSession,
-  TokenSet,
-  type DefaultSession,
-  type NextAuthOptions,
+	getServerSession,
+	type DefaultSession,
+	type NextAuthOptions,
 } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { env } from "../env.mjs";
@@ -18,19 +17,19 @@ import { prisma } from "./db";
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  **/
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      accessToken: string | undefined | null
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+	interface Session extends DefaultSession {
+		user: {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession["user"];
+		accessToken: string | undefined | null;
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -40,40 +39,40 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  **/
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    SpotifyProvider({
-      clientId: env.SPOTIFY_CLIENT_ID,
-      clientSecret: env.SPOTIFY_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: `user-read-currently-playing playlist-read-collaborative
+	callbacks: {
+		async session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		},
+	},
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		SpotifyProvider({
+			clientId: env.SPOTIFY_CLIENT_ID,
+			clientSecret: env.SPOTIFY_CLIENT_SECRET,
+			authorization: {
+				params: {
+					scope: `user-read-currently-playing playlist-read-collaborative
  user-read-recently-played user-top-read user-read-email playlist-read-private
 `,
-        },
-      },
-    }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Spotify provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
-  ],
-  //   pages: {
-  //     signIn: "/auth/signin",
-  //   },
+				},
+			},
+		}),
+		/**
+		 * ...add more providers here
+		 *
+		 * Most other providers require a bit more work than the Spotify provider.
+		 * For example, the GitHub provider requires you to add the
+		 * `refresh_token_expires_in` field to the Account model. Refer to the
+		 * NextAuth.js docs for the provider you want to use. Example:
+		 * @see https://next-auth.js.org/providers/github
+		 **/
+	],
+	//   pages: {
+	//     signIn: "/auth/signin",
+	//   },
 };
 
 /**
@@ -83,8 +82,8 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  **/
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+	req: GetServerSidePropsContext["req"];
+	res: GetServerSidePropsContext["res"];
 }) => {
-  return getServerSession(ctx.req, ctx.res, authOptions);
+	return getServerSession(ctx.req, ctx.res, authOptions);
 };
