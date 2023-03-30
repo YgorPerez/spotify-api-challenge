e5322e2d0d9b4dc094d1e20439eb793c4804dc1d
ygorperez <perezygor@gmail.com>
2023-03-30 12:28:36 -0300

    useScrollRestoration refactored and now works with safari

--- src/hooks/useScrollRestoration.ts
+++ src/hooks/useScrollRestoration.ts
@@ -1,12 +1,10 @@
+/**
+ * Based on https://gist.github.com/claus/992a5596d6532ac91b24abe24e10ae81
+ * - see https://github.com/vercel/next.js/issues/3303#issuecomment-628400930
+ * - see https://github.com/vercel/next.js/issues/12530#issuecomment-628864374
+ */
 import Router, { useRouter } from 'next/router'
 import { useEffect } from 'react'
-
-function saveScrollPos(asPath: string) {
-  sessionStorage.setItem(
-    `scrollPos:${asPath}`,
-    JSON.stringify({ x: window.scrollX, y: window.scrollY }),
-  )
-}
 
 type ScrollPos =
   | {
@@ -16,6 +14,13 @@ type ScrollPos =
   | null
   | undefined
 
+function saveScrollPos(asPath: string) {
+  sessionStorage.setItem(
+    `scrollPos:${asPath}`,
+    JSON.stringify({ x: window.scrollX, y: window.scrollY }),
+  )
+}
+
 function restoreScrollPos(asPath: string) {
   const json = sessionStorage.getItem(`scrollPos:${asPath}`)
   const scrollPos = json ? (JSON.parse(json) as ScrollPos) : undefined
@@ -24,13 +29,20 @@ function restoreScrollPos(asPath: string) {
   }
 }
 
-export function useScrollRestoration() {
+export const useScrollRestoration = (): void => {
   const router = useRouter()
+
+  useEffect(() => {
+    restoreScrollPos(router.asPath)
+  })
+
   useEffect(() => {
     if (!('scrollRestoration' in window.history)) return
-    let shouldScrollRestore = false
-    window.history.scrollRestoration = 'manual'
-    restoreScrollPos(router.asPath)
+
+    const userAgent = window.navigator.userAgent.toLowerCase()
+    const isMobileSafari =
+      /safari/.test(userAgent) && /iphone|ipod|ipad/.test(userAgent)
+    window.history.scrollRestoration = isMobileSafari ? 'auto' : 'manual'
 
     const onBeforeUnload = (event: BeforeUnloadEvent) => {
       saveScrollPos(router.asPath)
@@ -41,30 +53,12 @@ export function useScrollRestoration() {
       saveScrollPos(router.asPath)
     }
 
-    const onRouteChangeComplete = (url: string) => {
-      if (shouldScrollRestore) {
-        shouldScrollRestore = false
-        /**
-         * Calling with relative url, not expected asPath, so this
-         * will break if there is a basePath or locale path prefix.
-         */
-        restoreScrollPos(url)
-      }
-    }
-
     window.addEventListener('beforeunload', onBeforeUnload)
     Router.events.on('routeChangeStart', onRouteChangeStart)
-    Router.events.on('routeChangeComplete', onRouteChangeComplete)
-    Router.beforePopState(() => {
-      shouldScrollRestore = true
-      return true
-    })
 
     return () => {
       window.removeEventListener('beforeunload', onBeforeUnload)
       Router.events.off('routeChangeStart', onRouteChangeStart)
-      Router.events.off('routeChangeComplete', onRouteChangeComplete)
-      Router.beforePopState(() => true)
     }
   }, [router])
 }
