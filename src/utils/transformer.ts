982fc92d53590ecb9d5aaf669af28276b6754d77
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-06-14 12:34:44 -0300

    updated transformer

--- src/utils/transformer.ts
+++ src/utils/transformer.ts
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { uneval } from 'devalue'
 import superjson from 'superjson'
 
 export const transformer = {
   input: superjson,
-  output: {
-    serialize: (object: any) => uneval(object),
-    // This `eval` only ever happens on the **client**
-    deserialize: (object: any) => eval(`(${object})`),
-  },
+  output: superjson,
 }
