ee7b52c5f13537339259e8924ccc1a2e14cda6ce
Ygor Perez de Oliveira <azaz22155@gmail.com>
2023-06-25 20:35:50 -0300

    fixed css

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,21 +2,20 @@ import { SignIn } from '@clerk/nextjs'
 import { type NextPage } from 'next'
 
 const SignInPage: NextPage = () => (
-  <div className='fixed translate-x-1/2 translate-y-2/3'>
-    <SignIn
-      appearance={{
-        elements: {
-          logoBox: 'justify-center',
-          formButtonPrimary:
-            'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
-        },
-      }}
-      path='/auth/sign-in'
-      routing='path'
-      signUpUrl='/auth/sign-up'
-      redirectUrl='/search'
-    />
-  </div>
+  <SignIn
+    appearance={{
+      elements: {
+        rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
+        logoBox: 'justify-center',
+        formButtonPrimary:
+          'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
+      },
+    }}
+    path='/auth/sign-in'
+    routing='path'
+    signUpUrl='/auth/sign-up'
+    redirectUrl='/search'
+  />
 )
 
 export default SignInPage
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -2,21 +2,20 @@ import { SignUp } from '@clerk/nextjs'
 import { type NextPage } from 'next'
 
 const SignUpPage: NextPage = () => (
-  <div className='fixed translate-x-1/2 translate-y-2/3'>
-    <SignUp
-      appearance={{
-        elements: {
-          logoBox: 'justify-center',
-          formButtonPrimary:
-            'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
-        },
-      }}
-      path='/auth/sign-up'
-      routing='path'
-      signInUrl='/auth/sign-in'
-      redirectUrl='/search'
-    />
-  </div>
+  <SignUp
+    appearance={{
+      elements: {
+        rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
+        logoBox: 'justify-center',
+        formButtonPrimary:
+          'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
+      },
+    }}
+    path='/auth/sign-up'
+    routing='path'
+    signInUrl='/auth/sign-in'
+    redirectUrl='/search'
+  />
 )
 
 export default SignUpPage
