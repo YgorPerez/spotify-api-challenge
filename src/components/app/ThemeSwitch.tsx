e5a3ca7abcbb83828d95a2d5901b0563e41a6c63
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-07-08 10:47:44 -0300

    path fix

--- src/components/app/ThemeSwitch.tsx
+++ src/components/app/ThemeSwitch.tsx
@@ -1,13 +1,13 @@
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Button } from '@components/ui/button';
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
