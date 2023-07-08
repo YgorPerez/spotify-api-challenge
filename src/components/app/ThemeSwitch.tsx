eb0c8ce2bc5460b3500d36c91c112927d979ac7b
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-07-08 10:51:18 -0300

    name change

--- src/components/app/ThemeSwitch.tsx
+++ src/components/app/ThemeSwitch.tsx
@@ -1,13 +1,13 @@
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
+import { Button } from '@components/ui/Button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Button } from '@components/ui/button';
+} from '@components/ui/DropdownMenu';
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
