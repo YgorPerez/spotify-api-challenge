5ea650c278630ef8fc2e91735eb1f257c86bc6b0
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-07-08 10:58:49 -0300

    change bg to white

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,21 +1,23 @@
-import { SignIn } from '@clerk/nextjs'
-import { type NextPage } from 'next'
+import { SignIn } from '@clerk/nextjs';
+import { type NextPage } from 'next';
 
 const SignInPage: NextPage = () => (
-  <SignIn
-    appearance={{
-      elements: {
-        rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
-        logoBox: 'justify-center',
-        formButtonPrimary:
-          'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
-      },
-    }}
-    path='/auth/sign-in'
-    routing='path'
-    signUpUrl='/auth/sign-up'
-    redirectUrl='/search'
-  />
-)
+  <div className='h-screen w-screen bg-white'>
+    <SignIn
+      appearance={{
+        elements: {
+          rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
+          logoBox: 'justify-center',
+          formButtonPrimary:
+            'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
+        },
+      }}
+      path='/auth/sign-in'
+      routing='path'
+      signUpUrl='/auth/sign-up'
+      redirectUrl='/search'
+    />
+  </div>
+);
 
-export default SignInPage
+export default SignInPage;
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,21 +1,23 @@
-import { SignUp } from '@clerk/nextjs'
-import { type NextPage } from 'next'
+import { SignUp } from '@clerk/nextjs';
+import { type NextPage } from 'next';
 
 const SignUpPage: NextPage = () => (
-  <SignUp
-    appearance={{
-      elements: {
-        rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
-        logoBox: 'justify-center',
-        formButtonPrimary:
-          'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
-      },
-    }}
-    path='/auth/sign-up'
-    routing='path'
-    signInUrl='/auth/sign-in'
-    redirectUrl='/search'
-  />
-)
+  <div className='h-screen w-screen bg-white'>
+    <SignUp
+      appearance={{
+        elements: {
+          rootBox: 'fixed -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2',
+          logoBox: 'justify-center',
+          formButtonPrimary:
+            'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
+        },
+      }}
+      path='/auth/sign-up'
+      routing='path'
+      signInUrl='/auth/sign-in'
+      redirectUrl='/search'
+    />
+  </div>
+);
 
-export default SignUpPage
+export default SignUpPage;
