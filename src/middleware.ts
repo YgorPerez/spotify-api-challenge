514f98dff0b487218c381affdcf319ce5fce7145
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-07-10 05:26:04 -0300

    removed ratelimiter on clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,7 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
-import { ratelimiter } from './server/lib/redis-ratelimit'
+import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware({
-  async beforeAuth(request, event) {
-    const userIp = request.headers.get('x-forwarded-for')?.split(',')[0]
-    await ratelimiter({ userId: userIp || '127.0.0.1', event })
-  },
-  async afterAuth(auth, request, event) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return redirectToSignIn({ returnBackUrl: request.url })
-    }
-    await ratelimiter({ userId: auth.userId || undefined, event })
-  },
-})
+export default authMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+};
