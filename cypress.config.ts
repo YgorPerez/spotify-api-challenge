f6cc661c3acb5d258fefbf66d28bfa3c6c0006cd
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-07-12 05:09:08 -0300

    fix cypress base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'iazx18',
   e2e: {
-    baseUrl: 'localhost:3000',
+    baseUrl: 'http://localhost:3000',
   },
 });
