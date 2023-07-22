fa4baf8b4db42bdd598fab52ca4f1d1a2549aaac
Ygor Perez de Oliveira <azaz22155@gmail.com>
2023-07-22 18:05:42 -0300

    Doesn't need to fetch accessToken everytime

--- src/server/lib/spotify-api.ts
+++ src/server/lib/spotify-api.ts
@@ -1,18 +1,17 @@
-import { SpotifyWebApi } from 'spotify-web-api-ts-edge'
-import { env } from '../../env.mjs'
+import { SpotifyWebApi } from 'spotify-web-api-ts-edge';
+import { env } from '../../env.mjs';
 
 export const globalForSpotifyClient = globalThis as unknown as {
-  spotifyApi: SpotifyWebApi
-}
+  spotifyApi: SpotifyWebApi;
+};
 
-export const spotifyClientOauth = (accessToken: string) => {
-  return (
-    globalForSpotifyClient.spotifyApi ||
-    new SpotifyWebApi({
-      accessToken: accessToken,
-      clientId: env.SPOTIFY_CLIENT_ID,
-      clientSecret: env.SPOTIFY_CLIENT_SECRET,
-      redirectUri: env.CLERK_REDIRECT_URI,
-  })
-  )
-}
+export const spotifyClientOauth = (accessToken?: string) => {
+  globalForSpotifyClient.spotifyApi = new SpotifyWebApi({
+    accessToken: accessToken,
+    clientId: env.SPOTIFY_CLIENT_ID,
+    clientSecret: env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: env.CLERK_REDIRECT_URI,
+  });
+
+  return globalForSpotifyClient.spotifyApi;
+};
