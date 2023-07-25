5335004d43d624ce7851d2e0c03249cb4cea8206
ygorp2 <55122zaza@gmail.com>
2023-07-25 12:24:50 -0300

    added pwa config

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -10,6 +10,51 @@ const config: DefaultSeoProps = {
   },
   additionalMetaTags: [
     {
+      name: 'application-name',
+      content: 'Music Api Challenge',
+    },
+    {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes',
+    },
+    {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'default',
+    },
+    {
+      name: 'apple-mobile-web-app-title',
+      content: 'Music Api Challenge',
+    },
+    {
+      name: 'format-detection',
+      content: 'telephone=no',
+    },
+    {
+      name: 'mobile-web-app-capable',
+      content: 'yes',
+    },
+    {
+      name: 'msapplication-config',
+      content: '/public/browserconfig.xml',
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#fafafa',
+    },
+    {
+      name: 'theme-color',
+      content: '#fafafa',
+    },
+    {
+      name: 'viewport',
+      content:
+        'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover',
+    },
+    {
+      name: 'stylesheet',
+      content: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+    },
+    {
       name: 'view-transition',
       content: 'same-origin',
     },
@@ -30,8 +75,43 @@ const config: DefaultSeoProps = {
     {
       rel: 'apple-touch-icon',
       href: 'https://i.imgur.com/VdjeMM7.png',
-      sizes: '32x32',
       type: 'image/png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/public/images/touch-icon-ipad.png',
+      type: 'image/png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/public/images/touch-icon-ipad.png',
+      type: 'image/png',
+      sizes: '152x152',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/public/images/touch-icon-iphone-retina.png',
+      type: 'image/png',
+      sizes: '180x180',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/public/images/touch-icon-ipad-retina.png',
+      type: 'image/png',
+      sizes: '167x167',
+    },
+    {
+      rel: 'manifest',
+      href: '/public/manifest.json',
+    },
+    {
+      rel: 'shortcut',
+      href: 'https://i.imgur.com/DXIslqf.png',
+    },
+    {
+      rel: 'mask-icon',
+      href: 'https://i.imgur.com/DXIslqf.png',
+      color: '#fafafa',
     },
   ],
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,57 @@
 import NextBundleAnalyzer from '@next/bundle-analyzer';
 import { withAxiom } from 'next-axiom';
 import nextTranslate from 'next-translate-plugin';
+import { createRequire } from 'node:module';
 import { env } from './src/env.mjs';
+const require = createRequire(import.meta.url);
 
 const bundleAnalyzer = NextBundleAnalyzer({
   enabled: env.ANALYZE == true,
 });
 
-/** @type {import("next").NextConfig} */
-const config = withAxiom({
-  reactStrictMode: true,
-  ...nextTranslate(),
-  swcMinify: true,
-  optimizeFonts: true,
-  images: {
-    domains: ['i.scdn.co', 'api.spotify.com', 'api.clerk.dev', 'google.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-      },
-    ],
-    minimumCacheTTL: 1500000,
-  },
-  eslint: {
-    dirs: ['src'],
-  },
-  sassOptions: {
-    includePaths: ['./src/styles'],
-  },
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development',
 });
 
-export default bundleAnalyzer(config);
+/** @type {import("next").NextConfig} */
+const config = withPWA(
+  defineNextConfig({
+    reactStrictMode: true,
+    ...nextTranslate(),
+    swcMinify: true,
+    optimizeFonts: true,
+    images: {
+      domains: ['i.scdn.co', 'api.spotify.com', 'api.clerk.dev', 'google.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.scdn.co',
+        },
+      ],
+      minimumCacheTTL: 1500000,
+    },
+    eslint: {
+      dirs: ['src'],
+    },
+    sassOptions: {
+      includePaths: ['./src/styles'],
+    },
+  }),
+);
+
+export default bundleAnalyzer(withAxiom(config));
